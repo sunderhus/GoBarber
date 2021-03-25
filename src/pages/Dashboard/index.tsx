e335c3b8fb3c017b7a8abdbee97301b86ee6193e
Matheus Sunderhus <matheus.sunderhus@gmail.com>
2021-03-25 15:14:01 -0300

    feat: new dashboard header

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,16 +1,33 @@
 import React from 'react';
+import { FiPower } from 'react-icons/fi';
 import { useAuth } from '../../hooks/auth';
 
+import { Container, Header, HeaderContent, Profile } from './styles';
+
+import logoImg from '../../assets/logo.svg';
+
 const Dashboard: React.FC = () => {
-  const { signOut } = useAuth();
+  const { signOut, user } = useAuth();
 
   return (
-    <>
-      <h1>DashBoard</h1>
-      <button type="button" onClick={signOut}>
-        voltar
-      </button>
-    </>
+    <Container>
+      <Header>
+        <HeaderContent>
+          <img src={logoImg} alt="Logo GoBarber" />
+          <Profile>
+            <img src={user.avatarUrl} alt="Matheus Sunderhus" />
+            <div>
+              <span>Bem-vindo</span>
+              <b>{user.name}</b>
+            </div>
+          </Profile>
+
+          <button type="button" onClick={signOut}>
+            <FiPower />
+          </button>
+        </HeaderContent>
+      </Header>
+    </Container>
   );
 };
 
